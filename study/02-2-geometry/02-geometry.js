d0b52c0f58e5ee9e6bc197c757bdd78977c59eb0
letsjo <hyunoh.jo@gmail.com>
2023-01-22 15:04:25 +0900

    feat(*): geometry init

--- study/02-2-geometry/02-geometry.js
+++ study/02-2-geometry/02-geometry.js
@@ -36,9 +36,7 @@ class App {
     const width = this._divContainer.clientWidth;
     const height = this._divContainer.clientHeight;
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-
-    // 카메라와 시점의 사이 거리
-    camera.position.z = 15;
+    camera.position.z = 2;
     this._camera = camera;
   }
 
@@ -50,92 +48,54 @@ class App {
     this._scene.add(light);
   }
 
-  // Shape 클래스의 이해
-  // _setupModel() {
-  //   const shape = new THREE.Shape();
-  //   // 1,1 좌표로 이동
-  //   shape.moveTo(1, 1);
-  //   // 1,-1 좌표로 선을 그으면서 이동
-  //   shape.lineTo(1, -1);
-  //   shape.lineTo(-1, -1);
-  //   shape.lineTo(-1, 1);
-  //   // 시작점과 끝점을 이어서 도형을 완성시킨다.
-  //   shape.closePath();
-
-  //   const x = -2.5, y = -5;
-  //   shape.moveTo(x + 2.5, y + 2.5);
-  //   shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
-  //   shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
-  //   shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
-  //   shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
-  //   shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
-  //   shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
-
-  //   const geometry = new THREE.BufferGeometry();
-  //   const points = shape.getPoints();
-  //   geometry.setFromPoints(points);
-
-  //   const material = new THREE.LineBasicMaterial({ color: 0xffff00 });
-  //   const line = new THREE.Line(geometry, material);
-
-  //   this._scene.add(line);
-  // }
-
-  // Curve 클래스 이해
   _setupModel() {
-    class CustomSinCurve extends THREE.Curve {
-      constructor(scale) {
-        super();
-        this.scale = scale;
-      }
+    // 2차원 원판모양 4개 인자
+    // CircleGeometry (원판의 크기(1), 세그먼트수(8), 시작각도(0pi), 연장각도(2pi) )
+    // const geometry = new THREE.CircleGeometry(0.9, 16, Math.PI / 2, Math.PI / 2);
 
-      getPoint(t) {
-        const tx = t * 3 - 1.5;
-        const ty = Math.sin(2 * Math.PI * t);
-        const tz = 0;
-        return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
-      }
-    }
+    // 원뿔모양 7개 인자
+    // ConeGeometry (밑면 반지름크기(1), 원뿔의 높이(1), 원뿔의 둘레 세그먼트수(8), 원뿔 높이에 대한 세그먼트수(1), 원뿔밑면 개방여부(false), 원뿔의 시작각도(0pi), 원뿔의 연장각도(2pi) )
+    // const geometry = new THREE.ConeGeometry(0.5, 1.6, 16, 9, true, 0, Math.PI);
 
-    const path = new CustomSinCurve(4);
+    // 원통모양 8개 인자
+    // CylinderGeometry (윗면 반지름크기(1), 아랫면 반지름크기(1), 원통의 높이(1), 원통의 둘레 세그먼트수(8), 원통 높이에 대한 세그먼트수(1), 원통윗면아랫면 개방여부(false), 원뿔의 시작각도(0pi), 원뿔의 연장각도(2pi))
+    // const geometry = new THREE.CylinderGeometry(0.9, 0.9, 1.6, 32, 12, true, 0, Math.PI / 2);
 
-    const geometry = new THREE.BufferGeometry();
-    const points = path.getPoints();
-    geometry.setFromPoints(points);
+    // 구모양 7개 인자
+    // SphereGeometry (구의 반지름크기(1), 수평방향 세그먼트수(32), 수직방향 세그먼트수(16), 수평방향의 시작각도(0), 수평방향의 연장각도(2pi), 수직방향의 시작각도(0), 수직방향의 연장각도(2pi))
+    // const geometry = new THREE.SphereGeometry(0.9, 32, 32, 0, Math.PI, 0, Math.PI);
 
-    const material = new THREE.LineBasicMaterial({ color: 0xffff00 });
-    const line = new THREE.Line(geometry, material);
+    // 2차원형태 반지모양 6개 인자
+    // RingGeometry (내부 반지름값(0.5), 외부 반지름값(1), 가장자리 둘레방향 세그먼트수(8), 내부 방향에 대한 세그먼트수(1), 시작각도(0), 연장각도(2pi))
+    // const geometry = new THREE.RingGeometry(0.2, 1, 6, 2, 0, Math.PI);
 
-    this._scene.add(line);
+    // 2차원형태 평면 사각형 4개 인자
+    // 지리 정보 시스템 GIS 3차원 지형들을 표현하는데 사용됨
+    // PlaneGeometry (너비에 대한 길이(1), 높이에 대한 길이(1), 너비 세그먼트수(1), 높이 세그먼트수(1))
+    // const geometry = new THREE.PlaneGeometry(1, 1.4, 3, 2);
+
+    // 3차원형태 반지모양 5개 인자
+    // 긴 원통으로 360도 방향을 돌아서 이어진 모양
+    // TorusGeometry (반지름값(1), 원통에 반지름값(0.4), 방사 방향에 대한 세그먼트수(8), 원통 세그먼트수(6), 연장각도(2pi))
+    // const geometry = new THREE.TorusGeometry(0.9, 0.4, 24, 32, Math.PI);
+
+    // 3차원형태 고리모양 5개 인자
+    // 활용도가 떨어짐
+    // TorusKnotGeometry (크기의 반지름, 원통의 반지름, 크기의 세그먼트수, 원통의 세그먼트수, 반복횟수, 반복횟수)
+    const geometry = new THREE.TorusKnotGeometry(0.6, 0.1, 64, 32, 4, 6);
+    const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+    const cube = new THREE.Mesh(geometry, fillMaterial);
+
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
+    const line = new THREE.LineSegments(new THREE.WireframeGeometry(geometry), lineMaterial);
+
+    const group = new THREE.Group();
+    group.add(cube);
+    group.add(line);
+
+    this._scene.add(group);
+    this._cube = group;
   }
-
-  // _setupModel() {
-  //   const shape = new THREE.Shape();
-
-  //   const x = -2.5, y = -5;
-  //   shape.moveTo(x + 2.5, y + 2.5);
-  //   shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
-  //   shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
-  //   shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
-  //   shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
-  //   shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
-  //   shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
-
-  //   const geometry = new THREE.ShapeGeometry(shape);
-
-  //   const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
-  //   const cube = new THREE.Mesh(geometry, fillMaterial);
-
-  //   const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
-  //   const line = new THREE.LineSegments(new THREE.WireframeGeometry(geometry), lineMaterial);
-
-  //   const group = new THREE.Group();
-  //   group.add(cube);
-  //   group.add(line);
-
-  //   this._scene.add(group);
-  //   this._cube = group;
-  // }
 
   resize() {
     const width = this._divContainer.clientWidth;
